71e65a8e67baa468b3607759454adbe2cf890a55
Matheus Garcia Moreira <66276058+Matheusgarcia37@users.noreply.github.com>
2024-12-09 20:07:55 +0000

    modificando o versionamento com o `.gitignore`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home () {
     return (<div>
-        <h1>Aprenda como se você fosse viver para sempre. Viva como se você fosse morrer amanhã.</h1>
+      <h1>Aprenda como se você fosse viver para sempre. Viva como se você fosse morrer amanhã.</h1>
     </div>);
 }
 
