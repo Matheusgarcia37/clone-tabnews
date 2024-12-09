13909dacf60238068be323b23cde5a0b57b68e3a
Matheus Garcia Moreira <66276058+Matheusgarcia37@users.noreply.github.com>
2024-12-09 20:41:28 +0000

    adiciona scripts `lint:check` e `lint:fix` e formantando com o estilo do prettier o codigo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-function Home () {
-    return (<div>
-      <h1>Aprenda como se você fosse viver para sempre. Viva como se você fosse morrer amanhã.</h1>
-    </div>);
+function Home() {
+  return (
+    <div>
+      <h1>
+        Aprenda como se você fosse viver para sempre. Viva como se você fosse
+        morrer amanhã.
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
