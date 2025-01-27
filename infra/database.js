fbaab4bf0b1bcac5aec3825506f5a2df7af36153
Matheus Garcia Moreira <66276058+Matheusgarcia37@users.noreply.github.com>
2025-01-27 21:31:34 +0000

    make database.js more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,20 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
-
+  console.log("Credenciais de acesso ao banco: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
+    await client.connect();
     const res = await client.query(queryObject);
     return res;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
