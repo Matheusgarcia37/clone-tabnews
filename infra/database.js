12803dce257e1b35db979a114ec23b1b2e42c1b5
Matheus Garcia Moreira <66276058+Matheusgarcia37@users.noreply.github.com>
2025-01-27 20:53:06 +0000

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,17 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+
   await client.connect();
 
-  const res = await client.query(queryObject);
-
-  client.end();
-
-  return res;
+  try {
+    const res = await client.query(queryObject);
+    return res;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default { query };
