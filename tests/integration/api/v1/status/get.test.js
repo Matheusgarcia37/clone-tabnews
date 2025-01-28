af7bc66e3bd4e4cb0548db2a63ad45cbdf7683b3
Matheus Garcia Moreira <66276058+Matheusgarcia37@users.noreply.github.com>
2025-01-28 14:14:16 +0000

    update database configs toneon working

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -17,7 +17,7 @@ test("GET to /api/v1/status should return 200", async () => {
     0,
   );
   expect(responseBody.dependencies.database.version).toBeDefined();
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("16.6");
 
   expect(
     responseBody.dependencies.database.opened_connections,
